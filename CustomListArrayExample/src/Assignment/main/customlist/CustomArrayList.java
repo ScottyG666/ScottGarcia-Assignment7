1ae098801c7a6fa99327b1bc913d8cadd13aa8ce
ScottyP666 <Scott_M_G@live.com>
2022-02-07 23:20:01 -0800

    Assignment 7 Submission
    Creation of a custom array list using JUnit to implement  Test Driven Development.

---
+++ CustomListArrayExample/src/Assignment/main/customlist/CustomArrayList.java
@@ -0,0 +1,112 @@
+package Assignment.main.customlist;
+
+public class CustomArrayList<T> implements CustomList<T> {
+	Object[] items = new Object[10];
+	int size = 0;
+
+	
+	@Override
+	public boolean add(T item) {
+		if (items.length == size) {
+			growBackingArray();
+
+			items[size++] = item;
+			return true;
+
+		} else if (size < items.length) {
+
+			items[size++] = item;
+			return true;
+
+		} else {
+			return false;
+
+		}
+
+	}
+
+	@Override
+	public boolean add(int index, T item) throws IndexOutOfBoundsException {
+
+		if (index <= size) {
+			Object[] placeHolder = new Object[size + 1];
+
+			for (int i = 0; i < index; i++) {
+				placeHolder[i] = items[i];
+			}
+			placeHolder[index] = item;
+
+			for (int i = index + 1; i < placeHolder.length; i++) {
+				placeHolder[i] = items[i - 1];
+			}
+			items = placeHolder;
+			size++;
+			return true;
+
+		} else {
+			return false;
+		}
+
+	}
+
+	// this code works as expected
+	@Override
+	public int getSize() {
+		return size;
+	}
+
+	// this code works as expected
+	@SuppressWarnings("unchecked")
+	@Override
+	public T get(int index) throws IndexOutOfBoundsException {
+		return (T) items[index];
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T remove(int index) throws IndexOutOfBoundsException {
+
+		if (index <= size) {
+			T itemToRemove = (T) items[index];
+
+			size--;
+
+			Object[] placeHolderArray = new Object[size];
+
+			// places the objects in the items Array up until the point of the identified Object
+			// to be removed
+			for (int i = 0; i < index; i++) {
+				placeHolderArray[i] = items[i];
+			}
+
+			// places the objects in the items Array after the specified index to be removed
+			for (int i = index + 1; i < placeHolderArray.length + 1; i++) {
+				placeHolderArray[i - 1] = items[i];
+			}
+
+			items = placeHolderArray;
+
+			return itemToRemove;
+		} else {
+
+			return null;
+		}
+
+	}
+
+	// accessory method for when the Array needs to grow to accomidate added
+	// elements
+	private void growBackingArray() {
+		Object[] placeHolderArray = new Object[size];
+		for (int i = 0; i < size; i++) {
+			placeHolderArray[i] = items[i];
+		}
+
+		items = new Object[size * 2];
+		for (int i = 0; i < size; i++) {
+			items[i] = placeHolderArray[i];
+		}
+
+	}
+
+}
---
+++ CustomListArrayExample/test/Assignment/main/customlist/CustomArrayListTest.java
@@ -0,0 +1,79 @@
+package Assignment.main.customlist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CustomArrayListTest {
+
+	@Test
+	void should_add_15_items_causing_array_to_grow() {
+
+		// Assign
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		for (int i = 0; i < 15; i++) {
+			sut.add(i + 1);
+		}
+		Integer expected12thElement = sut.get(11);
+
+		// Assert
+		assertEquals(12, expected12thElement);
+	}
+
+	@Test
+	void should_add_item_at_specified_index_to_existing_array() {
+		// Assign
+		CustomList<Integer> sut = new CustomArrayList<>();
+
+		// Act
+		for (int i = 0; i < 15; i++) {
+			sut.add(i + 1);
+		}
+
+		sut.add(7, 16);
+
+		
+		//Assert
+		for (int i = 0; i < sut.getSize(); i++) {
+
+			if (i == 7) {
+				assertEquals(16, sut.get(7));
+			} else if (i < 7) {
+				assertEquals(i + 1, sut.get(i));
+			} else {
+				assertEquals(i, sut.get(i));
+			}
+		}
+
+	}
+	
+	
+	@Test
+	void remove_item_at_specified_index_and_shuffle_items_accordingly () {
+		//Assign 
+		CustomList<Integer> sut = new CustomArrayList<>();
+		
+		for (int i = 0; i < 25; i++) {
+			sut.add(i + 1);
+		}
+		
+		@SuppressWarnings("unused")
+		Integer number14TakenAway = sut.remove(13);
+		
+		assertEquals(14, number14TakenAway);
+		
+		for (int i = 0; i < sut.getSize(); i++) {
+			
+			if(sut.get(i)< number14TakenAway) {
+				assertEquals(i + 1, sut.get(i));
+			} else {
+				assertEquals(i + 2, sut.get(i));
+			}
+		}
+		
+		
+	}
+
+}
